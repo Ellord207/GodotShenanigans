370a1d1fb2a270478ffc5c9e079206f81a4a92ae
Archana Maharjan <archana.maharjan@bentley.com>
2020-07-11 17:23:27 -0500

    added start and stop

--- Assets/BaldRudeMan.gd
+++ Assets/BaldRudeMan.gd
@@ -1,8 +1,14 @@
 extends Spatial
+var baldManLimbAnimations={};
+func _start():
+	for animation in baldManLimbAnimations.keys():
+		baldManLimbAnimations.get(animation).play(animation);
+
+func _stop():
+	for animation in baldManLimbAnimations.keys():
+		baldManLimbAnimations.get(animation).play_backwards(animation);
 
 func _ready():
-	var firstAnimation;
 	for cube in self.get_children():
-		for animation in cube.get_children():
-			firstAnimation=animation.get_animation_list()[0];
-			animation.play(firstAnimation);
+		for animation in cube.get_children():	
+			baldManLimbAnimations[animation.get_animation_list()[0]]=animation;
