443dd87224239d0f1639a81de7e5a0d872bd9ab8
Archana Maharjan <archana.maharjan@bentley.com>
2020-07-11 20:10:08 -0500

    added looping

--- Assets/BaldRudeMan.gd
+++ Assets/BaldRudeMan.gd
@@ -1,14 +1,20 @@
 extends Spatial
 var baldManLimbAnimations={};
 func _start():
+	var limbAnim;
 	for animation in baldManLimbAnimations.keys():
-		baldManLimbAnimations.get(animation).play(animation);
+		limbAnim=baldManLimbAnimations.get(animation);
+		limbAnim.get_animation(animation).set_loop(true);
+		limbAnim.play(animation);
 
 func _stop():
+	var limbAnim;
 	for animation in baldManLimbAnimations.keys():
-		baldManLimbAnimations.get(animation).play_backwards(animation);
+		limbAnim=baldManLimbAnimations.get(animation);
+		limbAnim.play_backwards(animation);
 
 func _ready():
 	for cube in self.get_children():
 		for animation in cube.get_children():	
 			baldManLimbAnimations[animation.get_animation_list()[0]]=animation;
+			_start();
